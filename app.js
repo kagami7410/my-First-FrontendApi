5cd29917c6cc0b9062300997ce3a8efe58e08e25
sujangurung <sujangurung@sujangurungsmbp.home>
2022-06-09 23:42:25 +0100

    flag image added

--- app.js
+++ app.js
@@ -10,13 +10,21 @@ document.getElementsByClassName('country-form')[0].addEventListener('submit', fu
 
     console.log(countryInput.value);   
     const updateCountryName = document.getElementById("countryName")
+    const updatePopulation = document.getElementById("population")
+    const updateFlag = document.getElementById("countrys_flag")
 
     fetch(`https://restcountries.com/v2/name/${countryInput.value}`)
     .then(response => response.json())
     .then(data => {
         countryName = data[0].name
+        population = data[0].population
+        flag = data[0].flag
         updateCountryName.innerHTML = countryName
-        console.log(countryName)
+        updatePopulation.innerHTML = population
+        updateFlag.src = flag
+
+        // console.log(countryName)
+        console.log(data)
     })
  
 })
